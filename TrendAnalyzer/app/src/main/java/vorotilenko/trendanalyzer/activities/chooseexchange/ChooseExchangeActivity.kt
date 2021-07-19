40d496a831875e862ff9cf5169a6364ae082b7af
Vorotilenko <47428389+P-Vorotilenko@users.noreply.github.com>
2021-07-19 16:42:59 +0300

    ChooseExchangeActivity updated
    Simple search bar added

--- TrendAnalyzer/app/src/main/java/vorotilenko/trendanalyzer/activities/chooseexchange/ChooseExchangeActivity.kt
+++ TrendAnalyzer/app/src/main/java/vorotilenko/trendanalyzer/activities/chooseexchange/ChooseExchangeActivity.kt
@@ -3,9 +3,14 @@ package vorotilenko.trendanalyzer.activities.chooseexchange
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
 import vorotilenko.trendanalyzer.R
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ChooseExchangeActivity : AppCompatActivity() {
 
@@ -23,6 +28,26 @@ class ChooseExchangeActivity : AppCompatActivity() {
         val rvExchangesList = findViewById<RecyclerView>(R.id.rvExchangesList)
         val adapter = ExchangeAdapter(applicationContext, shownExchanges)
         rvExchangesList.adapter = adapter
-        adapter.notifyDataSetChanged()
+
+        val etExchangeName = findViewById<EditText>(R.id.etExchangeName)
+        etExchangeName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val searched = s.toString()
+                shownExchanges.clear()
+                if (searched == "")
+                    shownExchanges.addAll(allExchanges)
+                else {
+                    allExchanges.forEach {
+                        if (it.name.lowercase().startsWith(searched.lowercase().trim()))
+                            shownExchanges.add(it)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+            }
+        })
     }
 }
